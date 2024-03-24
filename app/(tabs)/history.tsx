579ab0cb17bcbd0ba3ac3788f8afc98f29f40f91
Green Gem <chris_waitt@me.com>
2024-03-24 12:30:11 +0000

    Add new files and update configurations

---
+++ app/(tabs)/history.tsx
@@ -0,0 +1,30 @@
+import { Text, ScrollView, Button, Card, YStack } from 'tamagui'
+import { usePurchases, useDeletePurchase } from '../store';
+
+export default function TabHistoryScreen() {
+  const purchases = usePurchases();
+  const deletePurchase = useDeletePurchase();
+
+  return (
+    <ScrollView padding="$3">
+      <YStack gap="$3">
+        {purchases.map((purchase) => (
+          <Card key={purchase.id}>
+            <Card.Header padded>
+              <YStack gap="$2">
+                <Text>Amount: ${purchase.amount}</Text>
+                <Text>Category: {purchase.category}</Text>
+                <Text>Date: {purchase.date}</Text>
+                {purchase.note && <Text>Note: {purchase.note}</Text>}
+              </YStack>
+            </Card.Header>
+            <Card.Footer padded>
+              <Button onPress={() => deletePurchase(purchase.id)}>Delete Transaction</Button>
+            </Card.Footer>
+          </Card>
+          
+        ))}
+      </YStack>
+    </ScrollView>
+  )
+}
---
+++ app/(tabs)/new-transaction.tsx
@@ -0,0 +1,109 @@
+import React, { useState, useRef } from 'react';
+import { StyleSheet,  } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
+import { useAddPurchase } from '../store';
+import { ScrollView, Input, Button, Text, YStack } from 'tamagui'
+
+interface FormData {
+  amount: string;
+  category: string;
+  note: string;
+}
+
+export default function TabNewTransactionScreen() {
+  const [date, setDate] = useState(new Date());
+  const { control, handleSubmit, formState: { errors }, reset } = useForm<FormData>();
+
+  const amountInputRef = useRef<Input>(null);
+
+  const addPurchase = useAddPurchase();
+  
+
+  const onSubmit = (data: FormData) => {
+    addPurchase(Number(data.amount), data.category, date.toISOString(), data.note);
+    reset({
+      amount: '',
+      category: '',
+      note: ''
+    });
+    setDate(new Date());
+
+    if (amountInputRef.current) {
+      amountInputRef.current.focus();
+    }
+  };
+
+  const handleDateChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  };
+
+  return (
+    <ScrollView padding="$3">
+      <YStack gap="$2">
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input 
+              ref={amountInputRef}
+              placeholder='Amount'
+              value={value}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              keyboardType="numeric"
+            />
+          )}
+          name="amount"
+          rules={{ required: 'You must enter an amount' }}
+        />
+        {errors.amount && <Text style={styles.errorText}>{errors.amount.message}</Text>}
+
+        
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              placeholder="Category"
+            />
+          )}
+          name="category"
+          rules={{ required: 'You must enter a category' }}
+        />
+        {errors.category && <Text style={styles.errorText}>{errors.category.message}</Text>}
+
+        <DateTimePicker
+          value={date}
+          mode="date"
+          display="default"
+          onChange={handleDateChange}
+        />
+
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              placeholder="Note"
+            />
+          )}
+          name="note"
+        />
+
+        <Button onPress={handleSubmit(onSubmit)}>Add Transaction</Button>
+      </YStack>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  errorText: {
+    color: 'red',
+    marginBottom: 10,
+  },
+});
